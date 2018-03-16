744d249108dce41bb9b2c7be14fbe267fd74aff1
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2018-03-16 18:48:20 +0000

    update comments javascript

--- app.js
+++ app.js
@@ -23,33 +23,33 @@ var obstacles = [];
 var randomNumbers = [];
 var obstacleSpace = -50;
 
-for (var i = 0; i < 100; i++){
-	var newObstacle = new create_obstacle(0, obstacleSpace, canvasWidth, 10);
-	obstacleSpace -= 250;
-	obstacles.push(newObstacle);
-	randomNumbers.push(getRandomInt(60, canvasWidth - 250));
+for (var i = 0; i < 100; i++){ 
+	var newObstacle = new create_obstacle(0, obstacleSpace, canvasWidth, 10); // Nýtt obstacle
+	obstacleSpace -= 250; // Hvert nýtt obstacle verður 250px fyrir ofan fyrri obstacle
+	obstacles.push(newObstacle); // Nýtt obstacle settur í Array
+	randomNumbers.push(getRandomInt(60, canvasWidth - 250)); // Random tala sem fer í lista
 }
 
-function create_obstacle(x_postion, y_position, width, height){
+function create_obstacle(x_postion, y_position, width, height){ // Fall sem býr til obstacle
 	this.x_postion = x_postion,
 	this.y_position = y_position,
 	this.width = width,
 	this.height = height
 }    
-function getRandomInt(min, max) {
+function getRandomInt(min, max) { // Fall sem tekur inn lægstu oh hæstu tölu og finnur heila tölu á milli þeirra eða hæsta eða lægsta talan sjálf
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function render_obstacles(){
 	for (var i = 0; i < obstacles.length; i++){
-		if (obstacles[i].y_position >= canvasHeight){
-			context.clearRect(obstacles[i].x_postion, obstacles[i].y_position, obstacles[i].width, obstacles[i].height);
+		if (obstacles[i].y_position >= canvasHeight){ // Ef obstacles fara útaf 
+			context.clearRect(obstacles[i].x_postion, obstacles[i].y_position, obstacles[i].width, obstacles[i].height); 
 		}
 		else{
-			context.fill();
-			context.fillRect(obstacles[i].x_postion, obstacles[i].y_position += 1.5, obstacles[i].width, obstacles[i].height);
-			context.fillRect(playerXpostion, playerYpostion, 20, 20); // Kubburinn fær nýja staðsetningu
-			context.clearRect(randomNumbers[i], obstacles[i].y_position - 5, 200, 20);
+			context.fill(); // Fillir kubbinn og obstalces með ákveðnum lit
+			context.fillRect(obstacles[i].x_postion, obstacles[i].y_position += 1.5, obstacles[i].width, obstacles[i].height); // Obstacles að færast
+			context.fillRect(playerXpostion, playerYpostion, 20, 20); // Kubburinn að færast
+			context.clearRect(randomNumbers[i], obstacles[i].y_position - 5, 200, 20); // Eyðir hluta af obstacle
 		}
 	}
 }
