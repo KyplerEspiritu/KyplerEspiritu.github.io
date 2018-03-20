633d354330ea351ca0cdedf5f898e91e3ae2e14d
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2018-03-20 18:06:29 +0000

    message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var canvasWidth = context.canvas.width;
 var canvasHeight = context.canvas.height;
 
 var playerXpostion = 240;
-var playerYpostion = 800;
+var playerYpostion = 675;
 
 window.addEventListener("mousemove", function(e){
 	var mouseX = e.clientX - context.canvas.offsetLeft;
@@ -17,15 +17,18 @@ window.addEventListener("mousemove", function(e){
 
 }); // EventListener 'hlustar' á eða fylgist með hreyfingu músins
 
-var obstacles = [];  
-var randomNumbers = [];
-var obstacleSpace = -50;
+var obstacleHeight = 10;
+var obstacles = []; 
 
+var obstacleSpace = -50;
 for (var i = 0; i < 100; i++){ 
-	var newObstacle = new create_obstacle(0, obstacleSpace, canvasWidth, 10); // Nýtt obstacle
+	var obstacleWidthLeft = getRandomInt(60, canvasWidth - 250); 
+	var obstacleWidthSpace = obstacleWidthLeft + 100;
+	var leftObstacle = new create_obstacle(0, obstacleSpace, obstacleWidthLeft, obstacleHeight); // Nýtt obstacle
+	var rightObstacle = new create_obstacle(obstacleWidthSpace, obstacleSpace, canvasWidth, obstacleHeight); // Nýtt obstacle
 	obstacleSpace -= 250; // Hvert nýtt obstacle verður 250px fyrir ofan fyrri obstacle
-	obstacles.push(newObstacle); // Nýtt obstacle settur í Array
-	randomNumbers.push(getRandomInt(60, canvasWidth - 250)); // Random tala sem fer í lista
+	obstacles.push(leftObstacle); // Nýtt obstacle settur í Array
+	obstacles.push(rightObstacle); 
 }
 
 function create_obstacle(x_postion, y_position, width, height){ // Fall sem býr til obstacle
@@ -33,31 +36,30 @@ function create_obstacle(x_postion, y_position, width, height){ // Fall sem býr
 	this.y_position = y_position,
 	this.width = width,
 	this.height = height
-}    
+}
+ 
 function getRandomInt(min, max) { // Fall sem tekur inn lægstu oh hæstu tölu og finnur heila tölu á milli þeirra eða hæsta eða lægsta talan sjálf
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function render_obstacles(){
 	for (var i = 0; i < obstacles.length; i++){
-		if (obstacles[i].y_position >= canvasHeight){ // Ef obstacles fara útaf 
-			context.clearRect(obstacles[i].x_postion, obstacles[i].y_position, obstacles[i].width, obstacles[i].height); 
+		if (playerXpostion + 15 >= obstacles[i].x_postion && playerXpostion <= obstacles[i].x_postion + obstacles[i].width && playerYpostion >= obstacles[i].y_position && playerYpostion <= obstacles[i].y_position + obstacles[i].height){
+			context.clearRect(0, 0, canvasWidth, canvasHeight);
+			clearInterval(animationInterval);
+			break;
 		}
-		else{
-			context.fill(); // Fillir hringinn og obstalces með ákveðnum lit
-			context.fillStyle = "#035c70"; // Litur fyrir context
-			context.fillRect(obstacles[i].x_postion, obstacles[i].y_position += 1.5, obstacles[i].width, obstacles[i].height); // Obstacles að færast
-			context.fillStyle = "#989e00"; // Nýr litur
-			context.beginPath();
-			context.arc(playerXpostion, playerYpostion, 10, 0, 2 * Math.PI, false); // Hringurinn að færast
-			context.stroke();
-
-			context.clearRect(randomNumbers[i], obstacles[i].y_position - 5, 100, 20); // Eyðir hluta af obstacle
-		}
+		context.fillStyle = "#035c70"; // Litur fyrir context
+		context.fillRect(obstacles[i].x_postion, obstacles[i].y_position += 3, obstacles[i].width, obstacles[i].height); // Obstacles að færast
+		context.fillStyle = "#989e00"; // Nýr litur
+		context.beginPath();
+		context.fillRect(playerXpostion, playerYpostion, 15, 15); // Hringurinn að færast
+		context.stroke();
 	}
 }
+
 function animation(){ // Fall fyrir hreyfingar
 	context.clearRect(0, 0, canvasWidth, canvasHeight);
 	render_obstacles();
 }
-setInterval(animation, 5);
+var animationInterval = setInterval(animation, 10);
