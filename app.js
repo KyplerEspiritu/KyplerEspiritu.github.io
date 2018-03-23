14b5ac2bea5dd3feb62823a3c3fb65aff9d6d2a1
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2018-03-23 11:11:23 +0000

    javascript added score, changed element. css new class

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 let game = document.getElementById("game");   
 let canvas = document.getElementById("canvas");
 
@@ -13,19 +12,21 @@ let obstacleWidthSpace;
 let leftObstacle;
 let rightObstacle;
 
-
 // "GAME OVER" texti
 let div_message = document.createElement("div"); // Nýtt div element
 div_message.setAttribute("class", "message"); // Div element fær classan message
-let game_over_message = document.createTextNode("GAME OVER"); 
-div_message.appendChild(game_over_message); // Div element fær texta
+div_message.setAttribute("id", "message");
+
+// Stig notanda
+let div_score = document.createElement("span");
+div_score.setAttribute("class", "scoreDiv");
+div_score.setAttribute("id", "score");
 
 // "restart" takki
 let div_restart = document.createElement("span"); // Nýtt span element
-let restart_message = document.createTextNode("restart");
 div_restart.setAttribute("class", "restartDiv"); // Span element fær classan restartDiv
 div_restart.setAttribute("onClick", "window.location.reload()"); // Einning fær span element onclick sem refreshar síðuna
-div_restart.appendChild(restart_message); // Span element fær texta
+div_restart.setAttribute("id", "restart"); // Einning fær span element onclick sem refreshar síðuna
 
 let context = canvas.getContext("2d");
 let canvasWidth = context.canvas.width; 
@@ -35,7 +36,7 @@ let canvasHeight = context.canvas.height;
 let playerXpostion = 240; 
 let playerYpostion = 820;
 
-for (let i = 0; i < 7; i++){  // For loopa sem býr til 100 obstacles og lætur í obstacles listann
+for (let i = 0; i < 7; i++){  // For loopa sem býr til EINNIG 7 obstacles
 	obstacleWidthLeft = getRandomInt(60, canvasWidth - 250); // Random tala. Þetta er fyrir bilið að vera á random stað.
 	obstacleWidthSpace = obstacleWidthLeft + 100; // Bilið er 100px
 	leftObstacle = new create_obstacle(0, obstacleSpace, obstacleWidthLeft, obstacleHeight); // Vinstri obstacle
@@ -44,7 +45,6 @@ for (let i = 0; i < 7; i++){  // For loopa sem býr til 100 obstacles og lætur 
 	obstacles.push(leftObstacle); // Vinstri obstacle settur í Array
 	obstacles.push(rightObstacle);  // Hægri obstacle settur í Array
 }   
-
 
 function create_obstacle(x_postion, y_position, width, height){ // Fall sem býr til obstacle
 	this.x_postion = x_postion,
@@ -62,23 +62,30 @@ function render_obstacles(){
 		if (obstacles[i].y_position >= canvasHeight){ // Ef obstacles fara yfir útaf canvas boundaries
 			obstacles.splice(0, 2); // Eyðast það úr listanum og canvas
 
-			obstacleWidthLeft = getRandomInt(60, canvasWidth - 250); // Random tala. Þetta er fyrir bilið að vera á random stað.
-			obstacleWidthSpace = obstacleWidthLeft + 100; // Bilið er 100px
+
+			// Kóðinn hérna er fyrir þegar 1 obstacles fer út af canvasinn þá verður nýr obstacle
+			obstacleWidthLeft = getRandomInt(60, canvasWidth - 250); 
+			obstacleWidthSpace = obstacleWidthLeft + 100; 
 			obstacleSpace = -500;
-			leftObstacle = new create_obstacle(0, obstacleSpace, obstacleWidthLeft, obstacleHeight); // Vinstri obstacle
-			rightObstacle = new create_obstacle(obstacleWidthSpace, obstacleSpace, canvasWidth, obstacleHeight); // Hægri obstacle
-			obstacles.push(leftObstacle); // Vinstri obstacle settur í Array
-			obstacles.push(rightObstacle);  // Hægri obstacle settur í Array
+			leftObstacle = new create_obstacle(0, obstacleSpace, obstacleWidthLeft, obstacleHeight); 
+			rightObstacle = new create_obstacle(obstacleWidthSpace, obstacleSpace, canvasWidth, obstacleHeight); 
+			obstacles.push(leftObstacle); 
+			obstacles.push(rightObstacle); 
 
 		}
 		if (playerXpostion + 15 >= obstacles[i].x_postion && playerXpostion <= obstacles[i].x_postion + obstacles[i].width && playerYpostion >= obstacles[i].y_position && playerYpostion <= obstacles[i].y_position + obstacles[i].height){ // Ef þegar notandinn klessir á obstacles
-			game.appendChild(div_message); // "GAME OVER" texti birtist
-			game.appendChild(div_restart); // "restart" takki birtist
+			
 			canvas.setAttribute("class", "game"); // Canvas fær classan game sem minnkar opacity
+			game.appendChild(div_message); 
+			game.appendChild(div_score); 
+			game.appendChild(div_restart); 
+			document.getElementById("score").innerHTML = score;
+			document.getElementById("message").innerHTML = "GAME OVER";
+			document.getElementById("restart").innerHTML = "restart";
 			clearInterval(animationInterval); // Stoppar Interval
 		}
 		context.fillStyle = "#035c70"; // Litur fyrir context
-		context.fillRect(obstacles[i].x_postion, obstacles[i].y_position += 3.5, obstacles[i].width, obstacles[i].height); // Obstacles að færast
+		context.fillRect(obstacles[i].x_postion, obstacles[i].y_position += 3, obstacles[i].width, obstacles[i].height); // Obstacles að færast
 		context.fillStyle = "#989e00"; // Nýr litur
 		context.beginPath();
 		context.fillRect(playerXpostion, playerYpostion, 15, 15); // Hringurinn að færast
